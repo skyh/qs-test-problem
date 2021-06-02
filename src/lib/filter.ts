54012feac7239e1737d814f6dd3020b51534afdd
Andrey Ivlev <skyh@ya.ru>
2021-06-02 21:33:27 +0300

    Remove unused module
    src/lib/filter.ts

--- src/lib/filter.ts
+++
@@ -1,5 +0,0 @@
-export function *filter<T>(input: IterableIterator<T>, predicate: (item: T) => void | boolean): IterableIterator<T> {
-    for (const item of input) {
-        if (predicate(item)) yield item;
-    }
-}
